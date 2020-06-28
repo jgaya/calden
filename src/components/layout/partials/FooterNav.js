054012a5e3348f1a5d6b62dfd0fc2e9aefd3feb1
Gabriel <gabrielguarna@gmail.com>
2020-06-28 15:11:58 -0300

    more translations

--- src/components/layout/partials/FooterNav.js
+++ src/components/layout/partials/FooterNav.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import classNames from 'classnames';
 import { Link } from 'react-router-dom';
+import { FormattedMessage } from "react-intl";
 
 const FooterNav = ({
   className,
@@ -19,16 +20,16 @@ const FooterNav = ({
     >
       <ul className="list-reset">
         <li>
-          <Link to="#0">Contact</Link>
+          <Link to="#0"> <FormattedMessage id="footer.contact" defaultMessage="Contact" /> </Link>
         </li>
         <li>
-          <Link to="#0">About us</Link>
+          <Link to="#0"> <FormattedMessage id="footer.aboutus" defaultMessage="About us" /> </Link>
         </li>
         <li>
-          <Link to="#0">FAQ's</Link>
+          <Link to="#0"> <FormattedMessage id="footer.faq" defaultMessage="FAQ's" /> </Link>
         </li>
         <li>
-          <Link to="#0">Support</Link>
+          <Link to="#0"><FormattedMessage id="footer.support" defaultMessage="Support" /></Link>
         </li>
       </ul>
     </nav>
